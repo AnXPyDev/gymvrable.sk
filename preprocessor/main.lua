a7e372e505371c8d9fafb61883380d38e3022bbd
AnXPyDev <jkism332@gmail.com>
2021-10-22 12:18:58 +0200

    grid menu

---
+++ preprocessor/main.lua
@@ -0,0 +1,3 @@
+function xd
+    print("bruh")
+end
--- web/scripts/header-menu.js
+++ web/scripts/header-menu.js
@@ -1,6 +1,7 @@
 let sub_menu_names = [
-	"skola", "zamestnanci", "ziaci", "pk"
+	"skola", "studium", "pre-zaujemcov", "aktivity", "galeria", "kontakt"
 ];
+
 
 let sub_menu_count = sub_menu_names.length;
 
@@ -15,19 +16,16 @@ for (let i = 0; i < sub_menu_count; i++) {
 	let menu_item = document.getElementById("menu-item-" + menu_name);
 
 	menu_item.addEventListener("mouseenter", () => {
-		console.log("hover over " + menu_name);
-		console.log(sub_menus);
 
 		for (let j = 0; j < sub_menu_count; j++) {
 			sub_menus[j].style.display = "none";
 		}
 
 		sub_menu.style.display = "inherit";
+
 	});
 
 	menu_items.push(menu_item);
 	sub_menus.push(sub_menu);
 
 }
-
-sub_menus[0].style.display = "block";
