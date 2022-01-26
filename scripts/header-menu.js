0573b7526d1a68a54c9e64ed58bec1487c9ba514
AnXPyDev <jkism332@gmail.com>
2022-01-26 17:32:15 +0100

    Added things

--- scripts/header-menu.js
+++ scripts/header-menu.js
@@ -1,28 +1,20 @@
-let sub_menu_names = [
-	"skola", "zamestnanci", "ziaci", "studium", "bilingvalne", "komisie", "aktivity", "archiv", "kontakt"
-];
+let menu_items = Array.from(document.getElementsByClassName("menu-item"));
+let sub_menus = Array.from(document.getElementsByClassName("sub-menu-div"));
 
+let sub_menu_count = Math.min(menu_items.length, sub_menus.length);
 
-let sub_menu_count = sub_menu_names.length;
-
-let menu_items = [];
-let sub_menus = [];
+menu_items.splice(sub_menu_count);
+sub_menus.splice(sub_menu_count);
 
 for (let i = 0; i < sub_menu_count; i++) {
-	let menu_name = sub_menu_names[i];
-
-	let sub_menu = document.getElementById("sub-menu-" + menu_name);
-	let menu_item = document.getElementById("menu-item-" + menu_name);
+	let sub_menu = sub_menus[i];
+	let menu_item = menu_items[i];
 
 	menu_item.addEventListener("mouseenter", () => {
         active_sub_menu.style.display = "none";
 		sub_menu.style.display = "block";
         active_sub_menu = sub_menu
 	});
-
-	menu_items.push(menu_item);
-	sub_menus.push(sub_menu);
-
 }
 
 for (let sub_menu of sub_menus) {
--- scripts/selector.js
+++ scripts/selector.js
@@ -3,21 +3,43 @@ let contents = Array.from(document.getElementsByClassName("selector-content"));
 
 let count = Math.min(buttons.length, contents.length);
 
+let active_item = 0;
+
+let params = new URLSearchParams(document.location.search);
+let preselect = params.get("selector");
+
 contents.splice(count);
 
 for (let i = 0; i < count; i++) {
 	let button = buttons[i];
 	let content = contents[i];
 
-	button.onclick = () => {
-		for (let content of contents) {
-			content.style.display = "none";
-		}
+	if (preselect != null && button.getAttribute("selector-id") == preselect) {
+		active_item = i;
+	}
 
-		content.style.display = "block";
+	/*let href = button.getAttribute("selector-href");
+	let target = button.getAttribute("selector-target");*/
+
+	button.onclick = () => {
+		contents[active_item].style.display = "none";
+		buttons[active_item].classList.remove("selector-selected");
+
+		/*if (href != null) {
+			window.open(href, target);
+		}*/
+
+		button.classList.add("selector-selected");
+		content.style.display = "";
+		active_item = i;
 	}
 
 	content.style.display = "none";
 }
 
-contents[0].style.display = "block";
+contents[active_item].style.display = "";
+buttons[active_item].classList.add("selector-selected");
+
+for (let elm of Array.from(document.getElementsByClassName("selector-hide"))) {
+	elm.style.display = "none";
+}
