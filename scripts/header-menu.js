33ed73ba8aed95186e43aeab5d7a864de54327e8
AnXPyDev <jkism332@gmail.com>
2022-01-27 20:32:43 +0100

    biling

--- scripts/header-menu.js
+++ scripts/header-menu.js
@@ -10,11 +10,14 @@ for (let i = 0; i < sub_menu_count; i++) {
 	let sub_menu = sub_menus[i];
 	let menu_item = menu_items[i];
 
-	menu_item.addEventListener("mouseenter", () => {
+	let handler = () => {
         active_sub_menu.style.display = "none";
 		sub_menu.style.display = "block";
-        active_sub_menu = sub_menu
-	});
+        active_sub_menu = sub_menu;
+	}
+
+	menu_item.addEventListener("mouseenter", handler);
+	menu_item.addEventListener("click", handler); // qutebrowser triggers click event when using f to select
 }
 
 for (let sub_menu of sub_menus) {
